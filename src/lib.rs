ba5fb14f408598227926a7cc2db050c826245cbf
Luca Erbetta <luca.erbetta105@gmail.com>
2024-10-21 00:37:18 +0200

    Update lib.rs to reflect changes to protos, add helpers for conversion to/from nalgebra types

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,59 @@
 // Include the `items` module, which is generated from items.proto.
 // It is important to maintain the same structure as in the proto.
 
-use prost_reflect::DescriptorPool;
 use once_cell::sync::Lazy;
+use prost_reflect::DescriptorPool;
 
+pub mod math;
+pub mod nodes;
+pub mod parameters;
+pub mod plot;
 pub mod telemetry;
 pub mod utils;
-pub mod plot;
-pub mod parameters;
-pub mod nodes;
 
-pub mod quadcopter {
+pub mod crater_messages {
+    pub mod basic {
+        use nalgebra::{Matrix, RawStorage, Vector3, U1, U3, U4};
+        use num_traits::AsPrimitive;
+
+        include!(concat!(env!("OUT_DIR"), "/crater.basic.rs"));
+
+        impl<T: AsPrimitive<f32>, S: RawStorage<T, U3, U1>> From<Matrix<T, U3, U1, S>> for Vec3 {
+            fn from(v: Matrix<T, U3, U1, S>) -> Self {
+                Vec3 {
+                    x: v[0].as_(),
+                    y: v[1].as_(),
+                    z: v[2].as_(),
+                }
+            }
+        }
+
+        impl<T: AsPrimitive<f32>, S: RawStorage<T, U4, U1>> From<Matrix<T, U4, U1, S>> for Quaternion {
+            fn from(v: Matrix<T, U4, U1, S>) -> Self {
+                Quaternion {
+                    x: v[0].as_(),
+                    y: v[1].as_(),
+                    z: v[2].as_(),
+                    w: v[3].as_(),
+                }
+            }
+        }
+
+        impl<T: From<f32>> From<Vec3> for Vector3<T> {
+            fn from(v: Vec3) -> Self {
+                Vector3::new(v.x.into(), v.y.into(), v.z.into())
+            }
+        }
+
+        impl<T: From<f32>> From<Quaternion> for nalgebra::Quaternion<T> {
+            fn from(v: Quaternion) -> Self {
+                nalgebra::Quaternion::new(v.w.into(), v.x.into(), v.y.into(), v.z.into())
+            }
+        }
+    }
+
     pub mod sensors {
-        include!(concat!(env!("OUT_DIR"), "/quadcopter.sensors.rs"));
+        include!(concat!(env!("OUT_DIR"), "/crater.sensors.rs"));
     }
 }
 
@@ -22,4 +63,3 @@ pub static DESCRIPTOR_POOL: Lazy<DescriptorPool> = Lazy::new(|| {
     )
     .unwrap()
 });
-
