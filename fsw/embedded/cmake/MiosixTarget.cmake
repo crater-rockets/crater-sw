d5621fa03af983ae76b90e104832c23d15effe1c
Luca Erbetta <luca.erbetta105@gmail.com>
2025-03-08 22:29:59 +0100

    Update embedded setup

--- fsw/embedded/cmake/MiosixTarget.cmake
+++ fsw/embedded/cmake/MiosixTarget.cmake
@@ -1,8 +1,6 @@
 function (miosix_target TARGET)
-    target_link_libraries(${TARGET} PUBLIC
-        miosix stdc++ c m gcc atomic
-    )
-
+    target_link_libraries_system(${TARGET} PUBLIC miosix)
+    target_link_libraries(${TARGET} PUBLIC stdc++ c m gcc atomic)
     # Create .map file
     target_link_options(${TARGET} PRIVATE -Wl,-Map,$<TARGET_FILE_DIR:${TARGET}>/$<TARGET_FILE_BASE_NAME:${TARGET}>.map)
 
--- fsw/embedded/tests/tests_main.cpp
+++ fsw/embedded/tests/tests_main.cpp
@@ -1,14 +1,21 @@
 #include "lest/lest.hpp"
+#include <fmt/core.h>
 
-#define CASE( name ) lest_CASE( specification(), name )
+#define CASE(name) lest_CASE(specification(), name)
 
-lest::tests & specification()
+lest::tests& specification()
 {
     static lest::tests tests;
     return tests;
 }
 
-int main( int argc, char * argv[] )
+int main(int argc, char* argv[])
 {
-    return lest::run( specification(), argc, argv /*, std::cout */ );
+    int result = lest::run(specification(), argc, argv /*, std::cout */);
+
+    if (result == 0) {
+        fmt::println("Tests completed successfully");
+    } else {
+        fmt::println("Tests completed with ERRORS");
+    }
 }
